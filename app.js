a49cd83f4c6e896d550de0e077c545445ddd9275
ily-20131220 <ily20131220@gmail.com>
2023-12-14 16:22:38 +0800

    參數拿取完成

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const InputFile = document.querySelector("#UpFile");
+const InputFile = document.querySelector("#UpFile"); //input
+const MakeImage = document.querySelector("#Make"); //圖片log
 
 //預覽圖片
 InputFile.addEventListener("change", (e) => {
   let InputData = e.target.files[0];
   document.querySelector("#FileThumbnail").src = URL.createObjectURL(InputData);
 });
-
 
 // 讀取相片參數
 InputFile.addEventListener("change", (e) => {
@@ -15,14 +15,40 @@ InputFile.addEventListener("change", (e) => {
   EXIF.getData(ExifFile, function () {
     let ExifData = EXIF.getAllTags(this);
     console.log(ExifData);
-    FNumber = ExifData.FNumber;
-    setTimeout(() => {
-      let figure = document.querySelector("figure");
-      let ImageWidth = Image.scrollWidth;
-      let ImageHeight = Image.scrollHeight;
-      console.log(Image);
-      console.log("寬度", ImageWidth);
-      console.log("高度", ImageHeight);
-    }, 100);
+
+    //判斷是否能讀取到Make
+    if (ExifData.Make) {
+      Make = ExifData.Make.toLowerCase(); //將讀取到的廠牌換成小寫
+    } else {
+      console.log("讀取不到logo");
+    }
+    //判斷廠牌的型號給予對應的logo
+    if (Make == "canon") {
+      MakeImage.src = `./logo/${Make}.png`;
+    } else if (Make == "sony") {
+      MakeImage.src = `./logo/${Make}.png`;
+    } else {
+      console.log("不相同");
+    }
+    //拿取Model參數
+    const ModelTitel = document.querySelector("#Model");
+    let model = ExifData.Model;
+    ModelTitel.innerHTML = model;
+
+    //拿取光圈、快門、ISO、焦段
+    const FocalLengthText = document.querySelector("#FocalLength"); //焦段
+    let FocalLength = ExifData.FocalLength.numerator; //焦段
+    const ISOtext = document.querySelector("#ISO");
+    let ISO = ExifData.ISOSpeedRatings; //ISO
+    const ExposureTimeText = document.querySelector("#ExposureTime");
+    let ExposureTime = `${ExifData.ExposureTime.numerator}/${ExifData.ExposureTime.denominator}`; //快門
+    const FNumberText = document.querySelector("#FNumber");
+    let FNumber = `${ExifData.FNumber}`; //光圈
+    // console.log(FocalLength, ISO, ExposureTime, FNumber);
+    // console.log(FocalLengthText, ISOtext, ExposureTimeText, FNumberText);
+    if (FocalLength) FocalLengthText.innerText = `${FocalLength}mm`;
+    if (ISO) ISOtext.innerText = `ISO ${ISO}`;
+    if (ExposureTime) ExposureTimeText.innerText = ExposureTime;
+    if (FNumber) FNumberText.innerText = `F/${FNumber}`;
   });
 });
